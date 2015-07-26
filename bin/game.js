d7aff30d1c11a22a0d23c1b115c3311282ffffd4
Ricardo Barros <ricardofbarros@hotmail.com>
2015-07-26 16:13:19 +0100

    add cool intro

--- bin/game.js
+++ bin/game.js
@@ -1,10 +1,58 @@
+#!/usr/bin/env node
+
 // Dependencies
-var Board = require('Board');
-var Units = require('Units');
+// var Board = require('../lib/Board');
+// var Units = require('../lib/Units');
+var inquirer = require('inquirer');
+var figlet = require('figlet');
+var pkg = require('../package.json');
+var Menu = require('terminal-menu');
 
-var board = new Board() // dimensions
-var units = new Units() // must specify how many units and which
+figlet('Battleship', { font: 'Larry 3D 2', horizontalLayout: 'fitted'}, function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-units.export().forEach(function (unit) {
-  board.addUnit(unit);
+  var greetings = data + 'v' + pkg.version;
+  greetings = greetings.split(/\r?\n/);
+
+  var menu = Menu({ width: 100, x: 1, y: 2 });
+  menu.reset();
+
+  greetings.forEach(function (greeting) {
+    menu.write(greeting + '\n');
+  });
+  menu.write('\n');
+  menu.write('READY TO BOMB THE HELLOUT OF YOUR OPPONENT?\n');
+  menu.write('-------------------------------------------\n');
+
+  menu.add('START');
+  menu.add('OPTIONS');
+  menu.write('-------------------------------------------\n');
+  menu.add('EXIT');
+
+  menu.on('select', function (label) {
+    if (label === 'EXIT') {
+      menu.close();
+    }
+  });
+
+  process.stdin
+    .pipe(menu.createStream())
+    .pipe(process.stdout);
+
+  process.stdin.setRawMode(true);
+  menu.on('close', function () {
+    process.stdin.setRawMode(false);
+    process.stdin.end();
+  });
 });
+
+
+// var board = new Board() // dimensions
+// var units = new Units() // must specify how many units and which
+//
+// units.export().forEach(function (unit) {
+//   board.addUnit(unit);
+// });
